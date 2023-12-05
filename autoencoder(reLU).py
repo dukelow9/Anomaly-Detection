8e4e7e88cd41e487fc7b6da3de6bc79442fd4166
dukelow9 <104810932+dukelow9@users.noreply.github.com>
2023-12-05 20:40:45 +0800

    Update and rename autoencoder.py to autoencoder(reLU).py

--- autoencoder(reLU).py
+++ autoencoder(reLU).py
@@ -6,15 +6,22 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
 
 data = pd.read_excel("C:\\Users\\User\\OneDrive\\Documents\\Notes\\Year 3\\GP\\data1.xlsx")
-currency = 'JPY'
+currency = 'GBP'
 filteredData = data[data['Currency'] == currency]
 dates = filteredData['Date']
 spotRates = filteredData['Spot Rate']
 
 scaler = MinMaxScaler()
 spotRatesNormalized = scaler.fit_transform(np.array(spotRates).reshape(-1, 1))
+
+earlyStopping = EarlyStopping(
+    monitor='val_loss',
+    patience=45,
+    restore_best_weights=True
+)
 
 inputLayer = Input(shape=(1,))
 encoded = Dense(128, activation='relu')(inputLayer)
@@ -24,9 +31,10 @@ decoded = Dense(1, activation='linear')(encoded)
 autoencoder = Model(inputLayer, decoded)
 autoencoder.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error')
 autoencoder.summary()
-start_time = time.time()
-history = autoencoder.fit(spotRatesNormalized, spotRatesNormalized, epochs=150, batch_size=256, shuffle=False, validation_split=0.2)
-end_time = time.time()
+startTime = time.time()
+history = autoencoder.fit(spotRatesNormalized, spotRatesNormalized, epochs=1000, batch_size=256, shuffle=False,
+                          validation_split=0.2, callbacks=earlyStopping)
+endTime = time.time()
 
 encodedspotRatesNormalized = autoencoder.predict(spotRatesNormalized)
 encodedSpotRates = scaler.inverse_transform(encodedspotRatesNormalized)
@@ -34,11 +42,11 @@ encodedSpotRates = scaler.inverse_transform(encodedspotRatesNormalized)
 reconstructionErrors = np.mean(np.square(spotRatesNormalized - encodedspotRatesNormalized), axis=1)
 
 meanError = np.mean(reconstructionErrors)
-threshold = meanError + 4 * np.std(reconstructionErrors)
+threshold = meanError + 2 * np.std(reconstructionErrors)
 anomalies = np.where(reconstructionErrors > threshold)[0]
 
-elapsed_time = end_time - start_time
-print(f"Training Time: {elapsed_time} seconds")
+elapsedTime = endTime - startTime
+print(f"Training Time: {elapsedTime} seconds")
 
 plt.figure(figsize=(12, 6))
 plt.plot(dates, spotRates, label='Actual Spot Rates', color='navy')
@@ -56,7 +64,6 @@ plt.grid()
 plt.show()
 
 print("Number of anomalies:", len(anomalies))
-
 
 plt.figure(figsize=(12, 6))
 plt.plot(history.history['loss'], label='Training Loss', color='mediumpurple')
